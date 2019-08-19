ff98241c73d0dd51a439eaa85e729f8f952daa8d
Petar12349 <48982741+Petar12349@users.noreply.github.com>
2019-08-20 01:13:40 +0200

    Delete baza.py

--- baza.py
+++
@@ -1,8 +0,0 @@
-from sqlite3 import *
-
-konekcija = connect("bazapodataka.db")
-c = konekcija.cursor()
-
-c.execute('CREATE TABLE IF NOT EXISTS Registar (ime TEXT, prezime TEXT, mjesto TEXT, ulica TEXT, zupanija TEXT)')
-
-konekcija.close()
